d4888d733e4e728dbd7ff45b9ac115c0c7137e76
lwfe <leonardolwfe@gmail.com>
2022-04-20 19:51:18 -0300

    styles: sponsor home screen layout

--- src/screens/sponsorHomeScreen/container/index.tsx
+++ src/screens/sponsorHomeScreen/container/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {Menu} from '../../../components/menu';
+import {List} from '../../../components/list';
 
-import {Container, Title} from './styles';
+import {
+  Container,
+  HeaderContainer,
+  SponsorInfo,
+  Greetings,
+  SponsorName,
+  CandidaciesInfo,
+  InfoText,
+} from './styles';
 
 export const SponsorHomeScreen: React.FC = () => {
   return (
     <Container>
-      <Title>Sponsor Home Screen</Title>
+      <HeaderContainer>
+        <Menu isAbsolutePosition top={50} right={30} />
+        <SponsorInfo>
+          <Greetings>Boas vindas</Greetings>
+          <SponsorName>Ioasys!</SponsorName>
+        </SponsorInfo>
+        <CandidaciesInfo>
+          <InfoText>Você possui 6 perfis para analisar</InfoText>
+        </CandidaciesInfo>
+      </HeaderContainer>
+
+      <List data={[]} renderItem={() => null} />
     </Container>
   );
 };

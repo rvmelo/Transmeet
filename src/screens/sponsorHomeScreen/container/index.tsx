933c8da660e111153d06ea8d692313eb0c2408a3
Leonardo Marques <60831308+lwfe@users.noreply.github.com>
2022-04-20 22:15:48 -0300

    styles: sponsor home screen layout (#12)
    * styles: sponsor home screen layout
    
    * Add: user from redux

--- src/screens/sponsorHomeScreen/container/index.tsx
+++ src/screens/sponsorHomeScreen/container/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {useAppSelector} from '../../../redux/hooks';
+import {Menu} from '../../../components/menu';
+import {List} from '../../../components/list';
 
-import {Container, Title} from './styles';
+import {
+  Container,
+  HeaderContainer,
+  SponsorInfo,
+  Greetings,
+  SponsorName,
+  CandidaciesInfo,
+  InfoText,
+} from './styles';
 
 export const SponsorHomeScreen: React.FC = () => {
+  const {user} = useAppSelector(store => store.userReducer);
+
   return (
     <Container>
-      <Title>Sponsor Home Screen</Title>
+      <HeaderContainer>
+        <Menu isAbsolutePosition top={50} right={30} />
+        <SponsorInfo>
+          <Greetings>Boas vindas</Greetings>
+          <SponsorName>{user?.name}!</SponsorName>
+        </SponsorInfo>
+        <CandidaciesInfo>
+          <InfoText>Você possui 6 perfis para analisar</InfoText>
+        </CandidaciesInfo>
+      </HeaderContainer>
+
+      <List data={[]} renderItem={() => null} />
     </Container>
   );
 };
--- src/screens/sponsorHomeScreen/container/styles.tsx
+++ src/screens/sponsorHomeScreen/container/styles.tsx
@@ -1,11 +1,43 @@
+import {RFValue} from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
+import LinearGradient from 'react-native-linear-gradient';
 
 export const Container = styled.View`
   flex: 1;
-  align-items: center;
-  justify-content: center;
+  background-color: ${({theme}) => theme.colors.secondary};
 `;
 
-export const Title = styled.Text`
-  color: black;
+export const HeaderContainer = styled(LinearGradient).attrs({
+  colors: ['#1F478B', '#BB4981'],
+})`
+  padding: 0 ${RFValue(40)}px;
+  padding-top: ${RFValue(80)}px;
+`;
+
+export const SponsorInfo = styled.View`
+  align-items: flex-start;
+`;
+
+export const Greetings = styled.Text`
+  font-size: ${({theme}) => theme.fonts.bodyText.regular}px;
+  font-family: ${({theme}) => theme.fonts.family.regular400};
+  color: ${({theme}) => theme.colors.textWhite};
+`;
+
+export const SponsorName = styled.Text`
+  font-size: 40px;
+  font-family: ${({theme}) => theme.fonts.family.bold700};
+  color: ${({theme}) => theme.colors.textWhite};
+`;
+
+export const CandidaciesInfo = styled.View`
+  align-items: center;
+  justify-content: center;
+  padding: ${RFValue(40)}px;
+`;
+
+export const InfoText = styled.Text`
+  font-size: ${({theme}) => theme.fonts.bodyText.mediumBase}px;
+  font-family: ${({theme}) => theme.fonts.family.bold700};
+  color: ${({theme}) => theme.colors.textWhite};
 `;
